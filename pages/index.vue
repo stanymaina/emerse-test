86c094473983e5200e4d92706049b78ac9382592
Carl Stanley <stanleymaina.edu38@hotmail.com>
2019-08-22 17:38:32 +0100

    Fixed currency select and added symbols

--- pages/index.vue
+++ pages/index.vue
@@ -9,34 +9,6 @@
       sm8
       md6
     >
-      <div class="content-wrapper">
-        <!-- Displays if myData is not set. -->
-        <v-icon v-if="!myData"></v-icon>
-    
-        <div v-else class="my-real-content">
-          <div class="text-center">
-            <logo />
-            <vuetify-logo />
-          </div>
-          <div class="text-xs-center" style="width:90%;margin-top:20px;">
-            <div style="text-align:left;margin-bottom:10px;">Product description</div>
-            <vue-editor v-model="description" :editorToolbar="customToolbar"></vue-editor>
-            <v-image-input
-              v-model="imageData"
-              :image-quality="0.85"
-              clearable
-              uploadIcon="mdi-cloud-upload-outline"
-              rotateClockwiseIcon="mdi-rotate-right"
-              rotateCounterclockwiseIcon="mdi-rotate-left"
-              flipHorizontallyIcon="mdi-flip-horizontal"
-              flipVerticallyIcon="mdi-flip-vertical"
-            />
-          </div>
-        </div>
-      </div>
-      <v-card-title class="headline">
-        E-merse Test Platform for Plugins
-      </v-card-title>
         <content-loader
           v-if="!myData1"
           :height="260"
@@ -81,6 +53,34 @@
             />
           </div>
         </div>
+      <div class="content-wrapper">
+        <!-- Displays if myData is not set. -->
+        <v-icon v-if="!myData"></v-icon>
+    
+        <div v-else class="my-real-content">
+          <div class="text-center">
+            <logo />
+            <vuetify-logo />
+          </div>
+          <div class="text-xs-center" style="width:90%;margin-top:20px;">
+            <div style="text-align:left;margin-bottom:10px;">Product description</div>
+            <vue-editor v-model="description" :editorToolbar="customToolbar"></vue-editor>
+            <v-image-input
+              v-model="imageData"
+              :image-quality="0.85"
+              clearable
+              uploadIcon="mdi-cloud-upload-outline"
+              rotateClockwiseIcon="mdi-rotate-right"
+              rotateCounterclockwiseIcon="mdi-rotate-left"
+              flipHorizontallyIcon="mdi-flip-horizontal"
+              flipVerticallyIcon="mdi-flip-vertical"
+            />
+          </div>
+        </div>
+      </div>
+      <v-card-title class="headline">
+        E-merse Test Platform for Plugins
+      </v-card-title>
       <v-card-text>
         <v-row align="center"> 
           <v-col class="d-flex" cols="12" sm="6">
@@ -88,7 +88,7 @@
               item-text="name"
               item-value="abbr"
               :items="items"
-              label="Outlined style"
+              label="Currencies"
               outlined
             ></v-select>
           </v-col>
@@ -121,16 +121,16 @@ export default {
   data() {
     return {
       items: [
-        { abbr: 'USD', name: 'United States Dollar' },
-        { abbr: 'KES', name: 'Kenyan shilling' },
-        { abbr: 'EUR', name: 'Euro' },
-        { abbr: 'GBP', name: 'Pound sterling' },
-        { abbr: 'CHF', name: 'Swiss franc' },
-        { abbr: 'CAD', name: 'Canada Dollar' },
-        { abbr: 'JPY', name: 'Japanese yen' },
-        { abbr: 'AUD', name: 'Australian dollar' },
-        { abbr: 'INR', name: 'Indian rupee' },
-        { abbr: 'NGN', name: 'Nigerian naira' }
+        { abbr: 'USD', name: '$ - United States Dollar' },
+        { abbr: 'KES', name: 'Ksh - Kenyan shilling' },
+        { abbr: 'EUR', name: '€ - Euro' },
+        { abbr: 'GBP', name: '£ - Pound sterling' },
+        { abbr: 'CHF', name: 'Fr - Swiss franc' },
+        { abbr: 'CAD', name: 'C$ - Canada Dollar' },
+        { abbr: 'JPY', name: '¥ - Japanese yen' },
+        { abbr: 'AUD', name: 'A$ - Australian dollar' },
+        { abbr: 'INR', name: '₹ - Indian rupee' },
+        { abbr: 'NGN', name: '₦ - Nigerian naira' }
       ],
       customToolbar: [
         ["bold", "italic", "underline"],
